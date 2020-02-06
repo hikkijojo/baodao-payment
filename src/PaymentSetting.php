1f3e962f2a08b9394111ff42a2e4c93d74a6c445
Kent Ma <kent.ma@33tech.co>
2020-02-06 15:14:34 +0800

    update payment setting

--- src/PaymentSetting.php
+++ src/PaymentSetting.php
@@ -4,30 +4,35 @@ namespace Baodao\Payment;
 
 class PaymentSetting
 {
-    public $appNo; //應用代號
-    public $merchantNo; //商戶代號
+    //Must 有設定就必帶的欄位
+    //Optional 不帶有預設值的，我們也用不到，可以不帶
 
-    public $host; //連線位置
-    public $md5Key; //md5 加密 key
-    public $rsaPublicKey;
-    public $rsaPrivateKey;
+    public $appNo; //應用代號, Must
+    public $merchantNo; //商戶代號, Must
+    public $md5Key; //md5 加密 key, Must
 
-    public $orderNo; // 訂單號
-    public $orderAmount; // 訂單金額 單元元
-    public $orderTime; // 訂單日期 格式 DateTime，需自行 format
-    public $productCode; // 產品代號
-    public $productName; // 產品名稱
-    public $userNo; // 使用者代號
-    public $merchantIp; // 連線 ip
+    public $rsaPublicKey; //Must
+    public $rsaPrivateKey; //Must
 
-    public $thirdPartyType; // 三方支付類型
-    public $tradeType; // 第三方接口模式
+    public $orderNo; // 訂單號 Must
+    public $orderAmount; // 訂單金額 單位 元 Must
+    public $orderTime; // 訂單日期 格式 DateTime Must
 
-    public $notifyUrl; // 異步通知回調網址
-    public $redirectUrl; // 結帳後用戶轉向網址
+    public $merchantIp; // 連線 ip Must
+    public $thirdPartyType; // 三方支付類型 Must
+    public $tradeType; // 第三方接口模式 Must
 
-    public $bankCard; // 銀行卡號
-    public $bankCode; // 銀行代碼
+    public $notifyUrl; // 異步通知回調網址 Must
+    public $redirectUrl; // 結帳後用戶轉向網址 Must
+//----------------------------------------------------------------------------------------
+    public $bankCard; // 銀行卡號 Optional 表單有的話，就需要
+    public $bankCode; // 銀行代碼 Optional 表單有的話，就需要
+
+    public $productCode; // 產品代號 Optional
+    public $productName; // 產品名稱 Optional
+    public $userNo; // 使用者代號 Optional
+
+    public $host; //連線位置 Optional
 
     const MD5='MD5';
     const RSA='RSA';
@@ -50,6 +55,16 @@ class PaymentSetting
             if (property_exists($this, $newKey)) {
                 $this->{$newKey} = $val;
             }
+        }
+        $now = new \DateTime();
+        if(empty($this->productName)) {
+            $this->productName = "Product".$now->format("Ymdhis");
+        }
+        if(empty($this->productCode)) {
+            $this->productCode = $this->productName;
+        }
+        if(empty($this->userNo)) {
+            $this->userNo = "User".$now->format("Ymdhis");
         }
     }
 }
