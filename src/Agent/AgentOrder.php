60a0f53799f678761065426cab8a7af69a4d4bea
Kent Ma <kent.ma@33tech.co>
2020-05-15 10:47:37 +0800

    do not throw exception

--- src/Agent/AgentOrder.php
+++ src/Agent/AgentOrder.php
@@ -4,7 +4,32 @@ namespace Baodao\Payment\Agent;
 
 class AgentOrder
 {
+    const STATUS_OK = 1;
+    const STATUS_FAILED = 0;
+    public $agentOrderNo;
     public $amount;
     public $orderNo;
-    public $agentOrderNo;
+    private $failedMsg = '';
+    private $status = self::STATUS_FAILED;
+
+    public function isSuccessCreated(): bool
+    {
+        return $this->status == self::STATUS_OK;
+    }
+
+    public function setStatusOK()
+    {
+        $this->status = self::STATUS_OK;
+    }
+
+    public function setFailedMessage(string $str)
+    {
+        $this->status = self::STATUS_FAILED;
+        $this->failedMsg = $str;
+    }
+
+    public function getFailedMessage()
+    {
+        return $this->failedMsg;
+    }
 }
